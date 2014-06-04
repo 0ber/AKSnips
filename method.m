57cbd6bc2cb5737811ac4bd221228030498a186d
Alex Kalinkin <alex.k@ramotion.com>
2014-06-04 13:38:41 +0400

    method added

---
+++ method.m
@@ -0,0 +1,25 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1265\cocoasubrtf200
+{\fonttbl\f0\fmodern\fcharset0 Courier-Oblique;\f1\fmodern\fcharset0 Courier;}
+{\colortbl;\red255\green255\blue255;\red135\green136\blue117;\red38\green38\blue38;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\sl360
+
+\f0\i\fs24 \cf2 // method
+\f1\i0 \cf3 \
+
+\f0\i \cf2 // 
+\f1\i0 \cf3 \
+
+\f0\i \cf2 // Platform: All
+\f1\i0 \cf3 \
+
+\f0\i \cf2 // Language: Objective-C
+\f1\i0 \cf3 \
+
+\f0\i \cf2 // Completion Scope: Code Expression
+\f1\i0 \cf3 \
+\
+-(void)method \{\
+\}\
+}
