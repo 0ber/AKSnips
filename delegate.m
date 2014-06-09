8ec9a8c74742a034e57a3fa9ea8c45e3e33b28f4
Alex <kalinkin.aleksey@gmail.com>
2014-06-09 22:08:00 +0400

    delegate
    added delegate files

---
+++ delegate.m
@@ -0,0 +1,8 @@
+// delegate property
+// 
+// Platform: All
+// IDECodeSnippetCompletionPrefix: delegate
+// Language: Objective-C
+// Completion Scope: Code Expression
+
+@property (nonatomic, weak) id<<#class#>> delegate;
